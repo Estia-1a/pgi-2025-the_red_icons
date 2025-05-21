0d1d0620ba1068e129be568bd51c2558928b8d99
MathysBIDEAU <189092393+MathysBIDEAU@users.noreply.github.com>
2025-05-21 14:59:17 +0200

    test supression text

--- src/main.c
+++ src/main.c
@@ -15,7 +15,7 @@ int main(int argc, char **argv) {
    or 
    - Simply run your compiled project with start button on the blue menu bar at the bottom of the VS Code window.
    */
-con
+
   /*DO NOT EDIT THIS PART*/
   Config configuration ;
   parse_arguments( argc, argv, &configuration ) ;
