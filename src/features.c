72f8cc568fb2079bf0429ccc3e2244a005768330
MathysBIDEAU <189092393+MathysBIDEAU@users.noreply.github.com>
2025-06-17 00:40:34 +0200

    correction print-pixel -- pas besoin de formule pour parcourrire le tableau

--- src/features.c
+++ src/features.c
@@ -76,7 +76,7 @@ void print_pixel( char *source_path, int x, int y ){
     int resultat = read_image_data(source_path, &data, &width, &height, &channel_count);
     
     if (resultat){
-     n=(width*channel_count*(y-1))+((x-1)*channel_count);
+     
      printf ("print_pixel (%d, %d): %d, %d, %d \n", x, y, data[n], data[n+1], data[n+2]);
     }
     else {
